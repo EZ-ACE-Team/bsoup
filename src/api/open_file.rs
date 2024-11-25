05c1262c284a870b6014b5907a4aafbc42c9b3fc
OJS <onionpeeel@gmail.com>
2024-11-25 12:36:12 +0900

    fix | Directory move to file bug

--- src/api/open_file.rs
+++ src/api/open_file.rs
@@ -6,15 +6,14 @@ use std::fs::File;
 
 #[post("/admin/getReadfile")]
 pub async fn get_readfile(body: web::Json<ReqFile>) -> impl Responder {
-    let mut path = body.request_path.to_owned();
+    let path = body.request_path.to_owned();
     let file_name = &body.file_name;
-    // let file_type = &body.file_type;
-    path.push_str(&file_name);
 
-    let mut f = fs::read_to_string(&path)
-        .expect("Should have been able to read the file");
+    let new_path = Path::new(&path).join(file_name);
+
+    let mut f = fs::read_to_string(new_path).expect("Should have been able to read the file");
 
     println!("{:?}", f);
 
-    HttpResponse::Ok().body(path)
+    HttpResponse::Ok().body(f)
 }
